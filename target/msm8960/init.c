0af101157d91fc02aa26d16cd6c770e2b972d735
dawei.chien <dawei.chien@gigabyte.com>
2013-05-30 14:27:16 +0800

    Don't enter power off charging if boot_reason is either recovery mode or fastboot mode.
    Category: Power
    Bug: None
    
    Change-Id: I919076820fb38ffc65199048caab8340c5f32456

--- target/msm8960/init.c
+++ target/msm8960/init.c
@@ -232,7 +232,10 @@ unsigned check_reboot_mode(void)
 
 unsigned target_pause_for_battery_charge(void)
 {
-	if ((target_check_power_on_reason() == PWR_ON_EVENT_USB_CHG) && (check_reboot_mode() != 0x77665501))
+	if ((target_check_power_on_reason() == PWR_ON_EVENT_USB_CHG) 
+		&& (check_reboot_mode() != 0x77665500)
+		&& (check_reboot_mode() != 0x77665501)
+		&& (check_reboot_mode() != 0x77665502))
 		return 1;
 
 	return 0;
